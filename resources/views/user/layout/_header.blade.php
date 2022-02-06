cdf5e11f3f80ab2aeed6c93c6a7327b5c6188a97
amanrajtoppoart007 <42941876+amanrajtoppoart007@users.noreply.github.com>
2022-02-06 11:46:55 +0530

    Update _header.blade.php

--- resources/views/user/layout/_header.blade.php
+++ resources/views/user/layout/_header.blade.php
@@ -13,7 +13,6 @@
                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                         Logout
                     </a>
-
                     <form id="logout-form" action="{{ route('logout') }}" method="POST"
                           style="display: none;">
                         {{ csrf_field() }}

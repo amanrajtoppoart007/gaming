05d4f5de7a521cd27d080677ee03db7abbefb81f
amanrajtoppoart007 <42941876+amanrajtoppoart007@users.noreply.github.com>
2022-02-07 00:02:30 +0530

    Update index.blade.php

--- resources/views/admin/puzzle/index.blade.php
+++ resources/views/admin/puzzle/index.blade.php
@@ -30,40 +30,36 @@
                         <th>{{$puzzle->level}}</th>
                         <th>
                             @if($puzzle->getMedia('questions')->last())
-                                <img style="width: 100px;height: 100px;" src="{{$puzzle->getMedia('questions')->last()->getUrl()}}"/>
+                                <img style="width: 100px;height: 100px;" src="{{$puzzle->getMedia('questions')->last()->getUrl()}}" alt=""/>
                             @endif
                         </th>
                         <th>
                             @if($puzzle->getMedia('solutions')->last())
-                                <img style="width: 100px;height: 100px;" src="{{$puzzle->getMedia('solutions')->last()->getUrl()}}"/>
+                                <img style="width: 100px;height: 100px;" src="{{$puzzle->getMedia('solutions')->last()->getUrl()}}" alt=""/>
                             @endif
                         </th>
                         <th>{{$puzzle->created_at->format('d-m-Y')}}</th>
                         <th>
                             <a href="{{route('admin.puzzle.show',$puzzle->id)}}">
-                                <!--begin::Svg Icon | path: assets/media/icons/duotune/general/gen016.svg-->
 <span class="svg-icon svg-icon-primary svg-icon-2hx"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
 <path d="M15.43 8.56949L10.744 15.1395C10.6422 15.282 10.5804 15.4492 10.5651 15.6236C10.5498 15.7981 10.5815 15.9734 10.657 16.1315L13.194 21.4425C13.2737 21.6097 13.3991 21.751 13.5557 21.8499C13.7123 21.9488 13.8938 22.0014 14.079 22.0015H14.117C14.3087 21.9941 14.4941 21.9307 14.6502 21.8191C14.8062 21.7075 14.9261 21.5526 14.995 21.3735L21.933 3.33649C22.0011 3.15918 22.0164 2.96594 21.977 2.78013C21.9376 2.59432 21.8452 2.4239 21.711 2.28949L15.43 8.56949Z" fill="black"/>
 <path opacity="0.3" d="M20.664 2.06648L2.62602 9.00148C2.44768 9.07085 2.29348 9.19082 2.1824 9.34663C2.07131 9.50244 2.00818 9.68731 2.00074 9.87853C1.99331 10.0697 2.04189 10.259 2.14054 10.4229C2.23919 10.5869 2.38359 10.7185 2.55601 10.8015L7.86601 13.3365C8.02383 13.4126 8.19925 13.4448 8.37382 13.4297C8.54839 13.4145 8.71565 13.3526 8.85801 13.2505L15.43 8.56548L21.711 2.28448C21.5762 2.15096 21.4055 2.05932 21.2198 2.02064C21.034 1.98196 20.8409 1.99788 20.664 2.06648Z" fill="black"/>
 </svg></span>
-<!--end::Svg Icon-->
                             </a>
                             <a  href="{{route('admin.puzzle.edit',$puzzle->id)}}">
-                                <!--begin::Svg Icon | path: assets/media/icons/duotune/general/gen055.svg-->
 <span class="svg-icon svg-icon-success svg-icon-2hx"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
 <path opacity="0.3" fill-rule="evenodd" clip-rule="evenodd" d="M2 4.63158C2 3.1782 3.1782 2 4.63158 2H13.47C14.0155 2 14.278 2.66919 13.8778 3.04006L12.4556 4.35821C11.9009 4.87228 11.1726 5.15789 10.4163 5.15789H7.1579C6.05333 5.15789 5.15789 6.05333 5.15789 7.1579V16.8421C5.15789 17.9467 6.05333 18.8421 7.1579 18.8421H16.8421C17.9467 18.8421 18.8421 17.9467 18.8421 16.8421V13.7518C18.8421 12.927 19.1817 12.1387 19.7809 11.572L20.9878 10.4308C21.3703 10.0691 22 10.3403 22 10.8668V19.3684C22 20.8218 20.8218 22 19.3684 22H4.63158C3.1782 22 2 20.8218 2 19.3684V4.63158Z" fill="black"/>
 <path d="M10.9256 11.1882C10.5351 10.7977 10.5351 10.1645 10.9256 9.77397L18.0669 2.6327C18.8479 1.85165 20.1143 1.85165 20.8953 2.6327L21.3665 3.10391C22.1476 3.88496 22.1476 5.15129 21.3665 5.93234L14.2252 13.0736C13.8347 13.4641 13.2016 13.4641 12.811 13.0736L10.9256 11.1882Z" fill="black"/>
 <path d="M8.82343 12.0064L8.08852 14.3348C7.8655 15.0414 8.46151 15.7366 9.19388 15.6242L11.8974 15.2092C12.4642 15.1222 12.6916 14.4278 12.2861 14.0223L9.98595 11.7221C9.61452 11.3507 8.98154 11.5055 8.82343 12.0064Z" fill="black"/>
 </svg></span>
-<!--end::Svg Icon-->
                             </a>
-                            <a class="delete-puzzle" data-delete_url="{{route('admin.puzzle.destroy',$puzzle->id)}}" data-id="{{$puzzle->id}}" href="#"><!--begin::Svg Icon | path: assets/media/icons/duotune/general/gen027.svg-->
+                            <a class="delete-puzzle" data-delete_url="{{route('admin.puzzle.destroy',$puzzle->id)}}" data-id="{{$puzzle->id}}" href="#">
 <span class="svg-icon svg-icon-danger svg-icon-2hx"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
 <path d="M5 9C5 8.44772 5.44772 8 6 8H18C18.5523 8 19 8.44772 19 9V18C19 19.6569 17.6569 21 16 21H8C6.34315 21 5 19.6569 5 18V9Z" fill="black"/>
 <path opacity="0.5" d="M5 5C5 4.44772 5.44772 4 6 4H18C18.5523 4 19 4.44772 19 5V5C19 5.55228 18.5523 6 18 6H6C5.44772 6 5 5.55228 5 5V5Z" fill="black"/>
 <path opacity="0.5" d="M9 4C9 3.44772 9.44772 3 10 3H14C14.5523 3 15 3.44772 15 4V4H9V4Z" fill="black"/>
 </svg></span>
-<!--end::Svg Icon--></a>
+</a>
                         </th>
                     </tr>
                 @endforeach
@@ -159,7 +155,7 @@
                                 });
                         }
                     },
-                    error: function (jqXHR, textStatus, errorThrown) {
+                    error: function (jqXHR, textStatus) {
                         Swal.fire({
                             text: textStatus,
                             icon: "error",

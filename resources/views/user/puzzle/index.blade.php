caa9139c96249dbbdd582121c824e07d46f1532f
amanrajtoppoart007 <42941876+amanrajtoppoart007@users.noreply.github.com>
2022-02-07 11:14:05 +0530

    Update index.blade.php

--- resources/views/user/puzzle/index.blade.php
+++ resources/views/user/puzzle/index.blade.php
@@ -6,8 +6,7 @@
                 <div class="row">
                     @foreach($puzzles as $puzzle)
                     <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 me-n2 mb-5 text-center justify-content-center">
-                        <button type="button"
-                                class="btn btn-outline btn-bg-primary btn-color-info-600  border-dashed border-active border-primary px-6 py-7 text-start w-100 min-w-150px text-center">
+                        <button type="button" class="btn btn-outline btn-bg-primary btn-color-info-600  border-dashed border-active border-primary px-6 py-7 text-start w-100 min-w-150px text-center">
                                  <span class="text-white fw-boldest">Level {{$puzzle->level}}</span>
                                  <br/>
                                  <span class="text-white fw-boldest"></span>
